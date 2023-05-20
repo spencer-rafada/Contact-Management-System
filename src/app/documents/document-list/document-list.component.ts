8dd494e5e716705eb1975cf1a70b7f63ab44b358
spencer-rafada <spencer.byui@gmail.com>
2023-05-20 17:14:26 -0600

    week 5 documents

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DocumentService } from '../document.service';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.css']
+  styleUrls: ['./document-list.component.css'],
 })
-export class DocumentListComponent {
-
+export class DocumentListComponent implements OnInit {
+  documents: Document[] = [];
+  constructor(private documentService: DocumentService) {}
+  ngOnInit(): void {
+    this.documents = this.documentService.getDocuments();
+  }
 }
---
+++ src/app/documents/document.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+import { Document } from './document.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DocumentService {
+  documents: Document[] = [];
+
+  constructor() {
+    this.documents = MOCKDOCUMENTS;
+  }
+
+  getDocuments() {
+    return this.documents.slice();
+  }
+
+  getDocument(id: string) {
+    this.documents.forEach((document) => {
+      if (document.id === id) {
+        return document;
+      }
+    });
+    return null;
+  }
+}
