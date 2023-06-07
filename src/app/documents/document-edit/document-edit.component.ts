397ea5fcdf5f56aa46d659b948da6d20f3073779
spencer-rafada <spencer.byui@gmail.com>
2023-06-06 20:17:21 -0600

    implement field validation for doc edit form

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Document } from '../document.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-document-edit',
   templateUrl: './document-edit.component.html',
-  styleUrls: ['./document-edit.component.css']
+  styleUrls: ['./document-edit.component.css'],
 })
 export class DocumentEditComponent {
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
 
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+
+  onCancel() {}
 }
