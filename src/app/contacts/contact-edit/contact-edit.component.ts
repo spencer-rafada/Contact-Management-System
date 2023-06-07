723e7c89987d4922ada2677c588e9e06b5aa5488
spencer-rafada <spencer.byui@gmail.com>
2023-06-06 20:56:42 -0600

    implement contact edit validation

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-edit',
   templateUrl: './contact-edit.component.html',
-  styleUrls: ['./contact-edit.component.css']
+  styleUrls: ['./contact-edit.component.css'],
 })
 export class ContactEditComponent {
+  groupContacts: Contact[];
+  contact: Contact;
 
+  onSubmit(form: NgForm) {}
+
+  onCancel() {}
 }
