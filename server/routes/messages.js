7e3f2cb5959711e46157087f6013fea31721fb31
spencer-rafada <spencer.byui@gmail.com>
2023-06-28 22:57:08 -0600

    contact implemented

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -9,7 +9,7 @@ router.get("/", (req, res, next) => {
     .then((message) => {
       res.status(200).json({
         message: "Messages fetched successfully",
-        messages: message,
+        documents: message,
       });
     })
     .catch((error) => {
